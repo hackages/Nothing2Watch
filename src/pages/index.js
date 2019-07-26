36e26c1edaf3e3093e4b11c6c697ade7f91c0758
Timothée Simon <timothee@hackages.io>
2019-07-26 14:45:11 +0200

    Using button to redirect correctly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,36 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 
-const randomGenerator = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+const randomGenerator = (min, max, random = Math.random()) => {
+	return Math.floor(random * (max - min + 1)) + min
 }
 
-const IndexPage = () => {
-  return (
-  <StaticQuery
-    query={graphql`
-{
-  allBeJson {
-    edges {
-      node {
-        title,
-        netflixid
-      }      
-    }
-  }
-}
-    `}
-    render={data => {
-      const { allBeJson } = data
-      const { edges } = allBeJson
-      const randomPosition = randomGenerator(0, edges.length - 1)
-      const randomMovie = edges[randomPosition].node
-      return (
-      <a href={"https://www.netflix.com/watch/" + randomMovie.netflixid}>
-					{randomMovie.title}
-				</a>
-      )
-    }}
-  />
-  )
-}
+const IndexPage = () =>( 
+	<StaticQuery
+		query={graphql`
+		{
+			allBeJson {
+				edges {
+					node {
+						title,
+						netflixid
+					}      
+				}
+			}
+		}
+				`}
+				render={data => {
+					const { allBeJson } = data
+					const { edges } = allBeJson
+					const randomPosition = randomGenerator(0, edges.length - 1)
+					const randomMovie = edges[randomPosition].node
+					return (
+						<button onClick={() => window.location.replace("https://www.netflix.com/watch/" + randomMovie.netflixid)}>
+							{randomMovie.title}
+						</button>
+					)
+				}}
+			/>
+)
 
 export default IndexPage
