a1b41de6f6410ec72eb5bc4ce40dc9829da0ce7c
Timothée Simon <timothee@hackages.io>
2019-07-26 15:51:46 +0200

    Added image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,57 @@
-import React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import React, {useState, useEffect} from "react"
+import { graphql } from "gatsby"
+import axios from "axios"
 
 const randomGenerator = (min, max, random = Math.random()) => {
 	return Math.floor(random * (max - min + 1)) + min
 }
 
-const IndexPage = () =>( 
-	<StaticQuery
-		query={graphql`
+const IndexPage = ({data}) =>
+{
+
+	const [image, setImage] = useState(undefined)
+	const [randomMovie, setRandomMovie] = useState({})
+
+
+
+	useEffect(() => {
+		const {edges} = data.allBeJson
+		const randomPosition = randomGenerator(0, edges.length - 1)
+		setRandomMovie(edges[randomPosition].node)
+	},[data, setRandomMovie])
+
+		useEffect(() => {
+			axios({
+				url: `https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?t=images&q=${randomMovie.netflixid}`,
+				method: "GET",
+				headers: {
+					"X-RapidAPI-Host": "unogs-unogs-v1.p.rapidapi.com",
+				"X-RapidAPI-Key": "8c075e4d6bmsh0a82cb219e37f09p1ddb4ejsnf1a67c1548da",
+			},
+			}).then(x => {
+				const images = x && x.data && x.data.RESULTS
+				console.log("images", images)
+				if(!images.filter){
+					return
+				}
+				const boxArts = images && images.filter(a => a.image[0].type === "boxart")
+				console.log("boxart", boxArts)
+				setImage(boxArts[0].image[0].url)
+			})
+	}, [randomMovie, setImage])
+
+	return (
+		<>
+		<img src={image} alt={randomMovie.title}/>
+		<button onClick={() => window.location.replace("https://www.netflix.com/watch/" + randomMovie.netflixid)}>
+			{randomMovie.title}
+		</button>
+</>
+	)
+}
+
+
+export const query = graphql`
 		{
 			allBeJson {
 				edges {
@@ -18,19 +62,6 @@ const IndexPage = () =>(
 				}
 			}
 		}
-				`}
-				render={data => {
-					const { allBeJson } = data
-					const { edges } = allBeJson
-					const randomPosition = randomGenerator(0, edges.length - 1)
-					const randomMovie = edges[randomPosition].node
-					return (
-						<button onClick={() => window.location.replace("https://www.netflix.com/watch/" + randomMovie.netflixid)}>
-							{randomMovie.title}
-						</button>
-					)
-				}}
-			/>
-)
+`
 
 export default IndexPage
